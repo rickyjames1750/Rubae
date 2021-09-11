da1854f866772c2923a798b63b06b92d92d580dd
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-11 02:15:47 -0700

    This commit builds out the early stages of HomeViewController's login screen

--- Rubae/Controllers/Core Tabs/HomeViewController.swift
+++ Rubae/Controllers/Core Tabs/HomeViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Ricky Sparks on 9/10/21.
 //
 
-
+import FirebaseAuth
 import UIKit
 
 class HomeViewController: UIViewController {
@@ -13,7 +13,22 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        handleNotAuthenticated()
+        
     }
     
+    private func handleNotAuthenticated() {
+        // Check auth status
+        if Auth.auth().currentUser == nil {
+            //Show login
+            let loginVC = LoginViewController()
+            loginVC.modalPresentationStyle = .fullScreen
+            present(loginVC, animated: false)
+        }
+        
+    }
 }
