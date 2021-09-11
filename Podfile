9be1b45f98e85f8fa57d32372d5ac991e4d7259a
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-11 01:55:49 -0700

    This commit will setup firebase for the iOS app backend
    1,285 changed files.
    Sizable commit submission.

--- Podfile
+++ Podfile
@@ -1,10 +1,18 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
+pod 'Firebase/Analytics'
+
 target 'Rubae' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for Rubae
+pod 'Appirater'
+
+# Firebase 
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
+pod 'Firebase/Crashlytics'
 
 end
--- Rubae/Resources/AppDelegate.swift
+++ Rubae/Resources/AppDelegate.swift
@@ -5,15 +5,16 @@
 //  Created by Ricky Sparks on 9/10/21.
 //
 
+import Firebase
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
