2b4c970b7602be639003d2540106c40c56446767
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-13 21:44:20 -0700

    Update AuthManager.swift

--- Rubae/Resources/AuthManager.swift
+++ Rubae/Resources/AuthManager.swift
@@ -17,7 +17,16 @@ public class AuthManager {
         
     }
     
-    public func loginUser(username: String?, email: String?, password: String) {
-        
+    public func loginUser(username: String?, email: String?, password: String, completion: (Bool -> Void)) {
+        if let email = email {
+            // email login
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                guard authResult! = nil, error = nil else {
+                    
+                }
+            }
+        } else if let username = username {
+            // username login
+        }
     }
 }
