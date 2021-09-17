817fec6114e69e14effaab4b6fb0071f3a2db738
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-16 17:34:28 -0700

    Update LoginViewController.swift

--- Rubae/Controllers/Other/Onboarding/LoginViewController.swift
+++ Rubae/Controllers/Other/Onboarding/LoginViewController.swift
@@ -192,8 +192,35 @@ class LoginViewController: UIViewController {
             return
         }
         
-        // login functionality
+        var username: String?
+        var email: String?
         
+        if usernameEmail.contains(".") {
+            // email
+            email = usernameEmail
+        } else {
+            // username
+            username = usernameEmail
+        }
+        
+        
+        
+        AuthManager.shared.loginUser(username: username, email: email, password: password) { success in
+            DispatchQueue.main.async {
+                if success {
+                    // user logged in
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    // error occured
+                    let alert = UIAlertController(title: "Log in Error", message: "We were unable to log you in.",
+                                                  preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                    self.present(alert, animated: true)
+                }
+            }
+            
+            
+        }
         
         
     }
