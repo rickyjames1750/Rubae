ee7dff26bf315ec0bd17593fbf4b308df1114db3
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-18 20:53:22 -0700

    Update DatabaseManager.swift

--- Rubae/Resources/DatabaseManager.swift
+++ Rubae/Resources/DatabaseManager.swift
@@ -13,5 +13,11 @@ public class DatabaseManager {
     
     // MARK: - Public
     
-    
+    /// Check if username and email is available
+    /// -Parameters
+    ///  -email: String representing email
+    ///  username: String representing username
+    public func canCreateNewUser(with email: String, username: String, completion: (Bool) -> Void){
+        
+    }
 }
