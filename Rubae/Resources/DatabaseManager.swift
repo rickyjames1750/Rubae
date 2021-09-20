a6f82b3544ad776b3e1bc2adbbe4ad2f94dc98bb
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-19 19:48:36 -0700

    Update DatabaseManager.swift

--- Rubae/Resources/DatabaseManager.swift
+++ Rubae/Resources/DatabaseManager.swift
@@ -11,6 +11,7 @@ public class DatabaseManager {
     
     static let shared = DatabaseManager()
     
+    private let database = Database.database().reference()
     // MARK: - Public
     
     /// Check if username and email is available
@@ -21,11 +22,13 @@ public class DatabaseManager {
         
     }
     
-    /// Inserts new user data to database 
+    /// Inserts new user data to database
     /// -Parameters
     ///  -email: String representing email
     ///  username: String representing username
     public func insertNewUser(with email: String, username: String) {
-        
+        database.child(email).setValue(["username": username], withCompletionBlock:{ error, _ in 
+            
+        })
     }
 }
