c9c2550d26e30e9c5342d98a2ae8847a6f05b59e
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-20 20:04:07 -0700

    Update DatabaseManager.swift

--- Rubae/Resources/DatabaseManager.swift
+++ Rubae/Resources/DatabaseManager.swift
@@ -19,7 +19,7 @@ public class DatabaseManager {
     ///  -email: String representing email
     ///  username: String representing username
     public func canCreateNewUser(with email: String, username: String, completion: (Bool) -> Void){
-        
+        completion(true)
     }
     
     /// Inserts new user data to database
@@ -28,7 +28,7 @@ public class DatabaseManager {
     ///  username: String representing username
     ///  completion: Async callback for result if database entry succeeded
     public func insertNewUser(with email: String, username: String, completion: @escaping (Bool) -> Void) {
-        database.child(email).setValue(["username": username]){ error, _ in
+        database.child(email.safaDatabaseKey()).setValue(["username": username]){ error, _ in
             if error == nil {
                 // succeeded
                 completion(true)
