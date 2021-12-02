d802f5444f7d851d9a0614797a5b5cde612e613c
rickyjames1750 <rickyjames1750@gmail.com>
2021-12-02 14:29:35 -0800

    Update SettingsViewController.swift

--- Rubae/Controllers/Other/General/SettingsViewController.swift
+++ Rubae/Controllers/Other/General/SettingsViewController.swift
@@ -45,6 +45,6 @@ extension SettingsViewController: UITableViewDelegate, UITableView {
     }
     // more code here
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        tableView.deselectRow(at: <#T##IndexPath#>, animated: true)
     }
 }
