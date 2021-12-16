7ade59df8c64b0763e649ebfa8a84c1ca3b35f52
rickyjames1750 <rickyjames1750@gmail.com>
2021-12-15 20:19:11 -0800

    Update ProfileViewController.swift

--- Rubae/Controllers/Core Tabs/ProfileViewController.swift
+++ Rubae/Controllers/Core Tabs/ProfileViewController.swift
@@ -20,7 +20,7 @@ class ProfileViewController: UIViewController {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize
+        layout.itemSize = CGSize()
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         
         guard let collectionView = collectionView else {
