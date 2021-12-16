21fc2641c2f6b865b7d3ea14c5a8c5ddfe715fed
rickyjames1750 <rickyjames1750@gmail.com>
2021-12-15 19:13:31 -0800

    Update ProfileViewController.swift

--- Rubae/Controllers/Core Tabs/ProfileViewController.swift
+++ Rubae/Controllers/Core Tabs/ProfileViewController.swift
@@ -11,5 +11,18 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureNavigationBar()
     }
+    private func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"),style: .done, target: self, action: #selector(didTapSettingsButton))
+    }
+    
+    @objc private func didTapSettingsButton() {
+        let vc = SettingsViewController()
+        vc.title = "Settings"
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
 }
