b8a43b97787258a34956a9b36c7ee9a04b010800
rickyjames1750 <rickyjames1750@gmail.com>
2021-12-15 20:25:32 -0800

    Update ProfileViewController.swift

--- Rubae/Controllers/Core Tabs/ProfileViewController.swift
+++ Rubae/Controllers/Core Tabs/ProfileViewController.swift
@@ -20,9 +20,11 @@ class ProfileViewController: UIViewController {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize()
+        layout.itemSize = CGSize(width: view.width/3, height: view.width/3)
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
         guard let collectionView = collectionView else {
             return
         }
@@ -38,5 +40,22 @@ class ProfileViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
         
     }
+    
+}
+
+
+extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+    
     
 }
