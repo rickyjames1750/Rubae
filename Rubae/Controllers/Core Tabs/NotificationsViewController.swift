88bf4a7c55fb6577e7b5cf91b438ba2852131b36
rickyjames1750 <rickyjames1750@gmail.com>
2021-12-16 10:23:53 -0800

    Update NotificationsViewController.swift

--- Rubae/Controllers/Core Tabs/NotificationsViewController.swift
+++ Rubae/Controllers/Core Tabs/NotificationsViewController.swift
@@ -5,10 +5,18 @@
 //  Created by Ricky Sparks on 9/10/21.
 //
 
+
 import UIKit
 
 class NotificationsViewController: UIViewController {
 
+    private let tableView: UITableView = {
+      let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+      return tableView
+      
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Notifications"
