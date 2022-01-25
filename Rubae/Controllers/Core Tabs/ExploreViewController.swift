488e54feab755684ad0d1bd9ceebc38ebb6a3ddd
rickyjames1750 <rickyjames1750@gmail.com>
2022-01-24 23:46:36 -0800

    Update ExploreViewController.swift

--- Rubae/Controllers/Core Tabs/ExploreViewController.swift
+++ Rubae/Controllers/Core Tabs/ExploreViewController.swift
@@ -24,6 +24,7 @@ class ExploreViewController: UIViewController {
         
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
+        layout.sectionInset  = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView?.delegate = self
         collectionView?.dataSource = self
