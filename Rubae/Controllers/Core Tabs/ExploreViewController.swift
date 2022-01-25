af8617068d1602f8a9e9ec57dce32edf972e729b
rickyjames1750 <rickyjames1750@gmail.com>
2022-01-25 05:52:26 -0800

    Update ExploreViewController.swift

--- Rubae/Controllers/Core Tabs/ExploreViewController.swift
+++ Rubae/Controllers/Core Tabs/ExploreViewController.swift
@@ -35,3 +35,7 @@ class ExploreViewController: UIViewController {
     }
     
 }
+
+extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewFlowLayout {
+    //
+}
