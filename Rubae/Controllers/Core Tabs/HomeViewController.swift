b9ef3b0b4ab32e5cb37abac305d3adb95c3a069e
rickyjames1750 <rickyjames1750@gmail.com>
2022-01-31 13:56:43 -0800

    Update HomeViewController.swift

--- Rubae/Controllers/Core Tabs/HomeViewController.swift
+++ Rubae/Controllers/Core Tabs/HomeViewController.swift
@@ -18,7 +18,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //
+        view.addSubview(tableView)
         //
         
         
