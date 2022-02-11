38d79a44bf2faf431ab4e07185da551d4da87345
rickyjames1750 <rickyjames1750@gmail.com>
2022-02-10 17:26:43 -0800

    Update HomeViewController.swift

--- Rubae/Controllers/Core Tabs/HomeViewController.swift
+++ Rubae/Controllers/Core Tabs/HomeViewController.swift
@@ -62,6 +62,8 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
-    //
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
 
