2461b58c2c52fd52ccb864e72870492f471b80e8
rickyjames1750 <rickyjames1750@gmail.com>
2022-02-23 21:22:07 -0800

    Update IGFeedPostTableViewCell.swift

--- Rubae/Views/Cells/IGFeedPostTableViewCell.swift
+++ Rubae/Views/Cells/IGFeedPostTableViewCell.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class IGFeedPostTableViewCell: UITableViewCell {
 
-    //
-
+    static let identifier = "IGFeedPostTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    // remove later
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
