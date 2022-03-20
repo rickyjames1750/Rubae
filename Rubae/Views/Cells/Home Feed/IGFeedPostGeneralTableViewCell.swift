603666f1fcac7fa40e0b418382a31a0335b87968
rickyjames1750 <rickyjames1750@gmail.com>
2022-03-20 00:15:26 -0700

    Create IGFeedPostGeneralTableViewCell.swift

---
+++ Rubae/Views/Cells/Home Feed/IGFeedPostGeneralTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  IGFeedPostGeneralTableViewCell.swift
+//  Rubae
+//
+//  Created by Ricky Sparks on 3/18/22.
+//
+
+import UIKit
+
+class IGFeedPostGeneralTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
