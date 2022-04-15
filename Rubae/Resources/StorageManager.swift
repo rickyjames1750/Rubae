ecd3869e50f62028fa0baf29b384f64b60270842
rickyjames1750 <rickyjames1750@gmail.com>
2022-04-14 23:30:14 -0700

    Update StorageManager.swift

--- Rubae/Resources/StorageManager.swift
+++ Rubae/Resources/StorageManager.swift
@@ -23,7 +23,7 @@ public class StorageManager {
         
         
     }
-    public func downloadImage(with reference: String, completion: (Result<URL, Error>) -> Void) {
+    public func downloadImage(with reference: String, completion: @esacaping (Result<URL, Error>) -> Void) {
         bucket.child(reference).downloadURL(completion: { url, error in guard let url = url, error == nil else {
             completion(.failure(.failedToDownload))
             return
