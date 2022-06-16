b5ce84a7bd22197b2777e4f480efb80a72a8ac4e
rickyjames1750 <rickyjames1750@gmail.com>
2022-06-16 03:12:46 -0700

    Update SettingsViewController.swift

--- Rubae/Controllers/Other/General/SettingsViewController.swift
+++ Rubae/Controllers/Other/General/SettingsViewController.swift
@@ -112,6 +112,8 @@ final class SettingsViewController: UIViewController {
     private func didTapEditProfile() {
         let vc = EditProfileViewController()
         vc.title = "Edit Profile"
+        let navVC = UINavigationController(rootViewController: vc)
+        
     }
     
     private func didTapLogOut(){
